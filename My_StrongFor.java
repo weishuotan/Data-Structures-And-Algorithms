2a57540bcd9cdc3785aae4f5e95c3c67c096fbd0
weishuotan <120439538+weishuotan@users.noreply.github.com>
2024-05-04 12:16:52 +0800

    Create My_StrongFor.java

---
+++ My_StrongFor.java
@@ -0,0 +1,25 @@
+package com.atguigu.my_set;
+
+import java.util.LinkedList;
+
+public class My_StrongFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<String> list = new LinkedList<>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("d");
+		for(String str: list) {
+			System.out.println(str);
+		}
+		
+		int[] arr = {1,2,3,4,5};
+		for(int i : arr) {
+			System.out.println(i);
+		}
+//		1.增强for遍历集合时,底层实现原理为迭代器
+//		2.增强for遍历数组时,底层实现原理为普通for
+	}
+}
